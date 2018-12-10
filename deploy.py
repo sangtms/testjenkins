1e2eb714723e67665c625e85b4a83f3b8a613c29
OMNICASADOM\sangtm <sangtm@omnicasa.com>
2018-12-10 14:53:22 +0700

    improve

---
+++ deploy.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import subprocess
+import codecs
+import json
+import builder
+import shutil
+import logging
+
+SEPERATE_LINE = '- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+
+print "sang test sang test"
+
+environment = ""
+if len(sys.argv) > 1:
+    environment = sys.argv[1]
+
+print "environment %s" %environment
+
+root_folder_path = os.path.dirname(os.path.realpath(__file__))
+print "root_folder_path %s" %root_folder_path
+
+solution_relative_file_path = r"TestJenkins.sln"
+print "Build solution %s" % solution_relative_file_path
+
+print SEPERATE_LINE
+
+#build and publish
+dotnet_builder = MsBuilder()
+buildOk = dotnet_builder.build_net_core(solution_relative_file_path)
+if not buildOk:
+	sys.exit(100)
