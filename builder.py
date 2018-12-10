7f5a62568e78c4052e4fb28e420b9d0c839a8546
OMNICASADOM\sangtm <sangtm@omnicasa.com>
2018-12-10 14:35:28 +0700

    impro

--- builder.py
+++ builder.py
@@ -1,1 +1,16 @@
-self.log('sang test sang test')
+import os
+import sys
+import subprocess
+import codecs
+import json
+import shutil
+import logging
+import requests
+
+print "sang test sang test"
+
+environment = ""
+if len(sys.argv) > 0:
+    environment = sys.argv[1]
+
+print "environment %s" %environment
